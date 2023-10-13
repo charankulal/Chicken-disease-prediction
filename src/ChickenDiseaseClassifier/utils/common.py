8e592190d63e7e3932adc5cd9a8f6249be470151
Charan <charankulal0241@gmail.com>
2023-10-13 22:30:29 +0530

    Done for the day

--- src/ChickenDiseaseClassifier/utils/common.py
+++ src/ChickenDiseaseClassifier/utils/common.py
@@ -35,8 +35,22 @@ def read_yaml(path_to_yaml:Path)->ConfigBox:
     except Exception as e:
         raise e
     
+    
 @ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """create list of directories
 
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+    
+@ensure_annotations
 def save_json(path:Path,data:dict):
     """save json data
 
