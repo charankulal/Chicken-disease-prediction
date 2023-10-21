533462ebf9b443e0b80e50907aaf3969ceb89750
Charan <charankulal0241@gmail.com>
2023-10-21 23:25:34 +0530

    Completed evaluation of the model and got the accuracy of 0.84 or 84%

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ChickenDiseaseClassifier import logger
 from ChickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from ChickenDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from ChickenDiseaseClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from ChickenDiseaseClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME="Data Ingestion Stage"
 try:
@@ -38,3 +39,15 @@ except Exception as e:
     logger.exception(e)
     raise e
         
+        
+STAGE_NAME = "Evaluation stage"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/ChickenDiseaseClassifier/config/configuration.py
+++ src/ChickenDiseaseClassifier/config/configuration.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 from ChickenDiseaseClassifier.constants import *
-from ChickenDiseaseClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
+from ChickenDiseaseClassifier.entity.config_entity import DataIngestionConfig, EvaluationConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
 from ChickenDiseaseClassifier.utils.common import read_yaml,create_directories
 
 class ConfigurationManager:
@@ -82,3 +83,13 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
